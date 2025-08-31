c49453f7e61c9b0ad312bc8dd4a523c50534a4cf
PravLab <tumhara-email@example.com>
2025-08-31 23:31:28 +0530

    this is day 2

--- app.js
+++ app.js
@@ -3,20 +3,121 @@ const menuBar = document.getElementById("menuBar");
 const menuMobileVersion = document.getElementById("menuMobileVersion");
 const linkOverview = document.getElementById("linkOverview");
 const collapseOverview = document.getElementById("collapseOverview");
+const collapseOff = document.getElementById("collapseOff");
+const addLink = document.querySelector(".add-link-btn");
+const contentSection = document.querySelector(".content-section");
+const inputBox = document.querySelectorAll(".input-box");
 
-
-menuBar.addEventListener("click",()=>{
-   menuMobileVersion.innerHTML=`
+menuBar.addEventListener("click", () => {
+  menuMobileVersion.innerHTML = `
    <ul id="menuItems">
             <li>About</li>
             <li>contact us</li>
             <li>How to use?</li>
-        </ul>`
+            </ul>`;
 
-     menuMobileVersion.classList.toggle("hidden");
+            menuMobileVersion.classList.toggle("hidden");
+});
+
+collapseOverview.addEventListener("click", () => {
+  collapseOverview.style.display = "none";
+  linkOverview.style.display = "block";
+});
+
+collapseOff.addEventListener("click", () => {
+    linkOverview.style.display = "none";
+  collapseOverview.style.display = "block";
 });
 
 
-collapseOverview.addEventListener("click",()=>{
-    linkOverview.classList.toggle("hidden");
+
+addLink.addEventListener("click", () => {
+    contentSection.innerHTML = `<div class="add-link-container">
+    
+    <!-- Link, Name for saving in specific category  -->
+            
+    <label >Name of category</label>
+    <input type="text" class="input-box" id="inputCategory" placeholder="Enter category (e.g. Podcast, Tool)" required>
+            <!-- enter link to save on local storage with category format. -->
+            <label>Paste your link</label>
+            <input type="text" class="input-box" id="inputLink" placeholder="Enter Your Link (e.g link2save.com)" required>
+            
+            <!-- some comment to knowing what that url for what the use of that. -->
+            <label>comment to know what the link does!</label>
+            <textarea name="" id="inputComment" placeholder="Enter something about the link"></textarea>
+            
+            <button id="saveLinkBtn">Save The Link</button>
+
+            <span class="error">please enter all the detail of your link to save</span>
+            
+            </div>`;
+            
+            
+                    
+                    const inputCategory = document.getElementById("inputCategory");
+                    const inputLink = document.getElementById("inputLink");
+                    const inputComment = document.getElementById("inputComment");
+                    const saveLinkBtn = document.getElementById("saveLinkBtn")
+                    
+                    
+                    
+            
+                    saveLinkBtn.addEventListener("click", () => {
+                        const category = inputCategory.value.trim();
+                        const link = inputLink.value.trim();
+                const comment = inputComment.value.trim();
+                
+                
+                let error = document.querySelector(".error");
+                if (!category || !link) {
+                    error.style.display="block"
+                    return;
+                } else {
+                    error.style.display="none"
+                }
+
+                let allData = JSON.parse(localStorage.getItem("allData")) || [];
+                
+                const linkData = {
+                   category: category,
+                   comment: comment,
+                   link: link
+                  };
+                  
+                allData.push(linkData);
+                
+                localStorage.setItem("allData",JSON.stringify(allData));
+
+                
+                
+                
+                document.querySelector(".add-link-container").style.display="none"
+                 
+                 let savedMsg = document.querySelector(".saved-message");
+                 savedMsg.style.display = "block";
+                
+                 setTimeout(() => {
+                    savedMsg.style.display = "none";
+                 }, 5000);
+ 
+                
+                
+            });
+        });
+        
+        
+
+const savedData =JSON.parse(localStorage.getItem("allData"))||[];
+
+
+
+console.log(savedData)
+
+savedData.map((item, index) => {
+ contentSection.innerHTML+=`<div class="all-links">
+    <h1>${item.category}</h1>
+    <p>${item.comment}</p>
+    <a href="${item.link}" target="_blank">${item.link}</a>
+  </div>
+ `
 });
